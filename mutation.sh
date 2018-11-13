6477a569103bc4fbd1b757f86172e4e3510b66d0
Anatolii Zamlynnyi <39742449+AnatoliiZamlynnyi@users.noreply.github.com>
2018-11-13 10:31:49 +0200

    Final scripts mutation

--- mutation.sh
+++ mutation.sh
@@ -1,7 +1,7 @@
 #! /bun/bash
 
 curl https://app.bitpool.com/#/login?pane=organisation >bitpool
-NUM=`grep -i "<div class="bpoAppVersion ng-binding">30</div>" bitpool | grep -Eo [0-9]+`
+NUM=`grep "bpoAppVersion ng-binding" bitpool |grep -Eo "[0-9]+"`
 HOUR=`date +"%H"`
 MINUTES=`date +"%M"`
 SUM=$(($HOUR+$MINUTES))
@@ -9,4 +9,5 @@ echo "Number from web resource app.bitpool.com" - "$NUM"
 echo "Current hours" - "$HOUR"
 echo "Current minutes" - "$MINUTES"
 echo "Summa current hours and current minutes - " $SUM
-echo "Mutation number from web resourse app.bitpool.com" - "$NUM+($HOUR+$MINUTES)=" $(($NUM+$SUM))
+echo "Mutation number from web resourse app.bitpool.com" - "$NUM+($HOUR+$MINUTES)="$(($NUM+$SUM))
+rm -f bitpool
